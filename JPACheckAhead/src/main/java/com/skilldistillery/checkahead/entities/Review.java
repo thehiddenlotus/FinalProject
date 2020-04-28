a45dc674ad654b9e3521381c4facf07c9af52104
Luke Sutherland <luke.sutherland@live.com>
2020-04-28 16:20:14 -0600

    all working entity relationships

--- JPACheckAhead/src/main/java/com/skilldistillery/checkahead/entities/Review.java
+++ JPACheckAhead/src/main/java/com/skilldistillery/checkahead/entities/Review.java
@@ -1,25 +1,25 @@
 package com.skilldistillery.checkahead.entities;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Review {
 	
+	// f i e l d s
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-
-//	@Column(name = "user_id")
-//	private int userId;
-//
-//	@Column(name = "location_id")
-//	private int locationId;
 
 	private String content;
 
@@ -33,6 +33,42 @@ public class Review {
 
 	@Column(name = "date_visited")
 	private LocalDateTime dateVistied;
+	
+	@ManyToOne
+	@JoinColumn(name="location_id")
+	private Location location;
+
+	@ManyToOne
+	@JoinColumn(name="user_id")
+	private User user;
+	
+	@OneToMany(mappedBy = "review")
+	private List<ReviewRating> ratings;
+
+	@OneToMany(mappedBy = "review")
+	private List<ReviewComment> comments;
+	
+	// m e t h o d s
+	
+	public Review() {
+		super();
+	}
+
+	public Review(int id, String content, boolean active, LocalDateTime dateCreated, LocalDateTime dateUpdated,
+			LocalDateTime dateVistied, Location location, User user, List<ReviewRating> ratings,
+			List<ReviewComment> comments) {
+		super();
+		this.id = id;
+		this.content = content;
+		this.active = active;
+		this.dateCreated = dateCreated;
+		this.dateUpdated = dateUpdated;
+		this.dateVistied = dateVistied;
+		this.location = location;
+		this.user = user;
+		this.ratings = ratings;
+		this.comments = comments;
+	}
 
 	public int getId() {
 		return id;
@@ -82,6 +118,67 @@ public class Review {
 		this.dateVistied = dateVistied;
 	}
 
+	public Location getLocation() {
+		return location;
+	}
 
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public List<ReviewRating> getRatings() {
+		return ratings;
+	}
+
+	public void setRatings(List<ReviewRating> ratings) {
+		this.ratings = ratings;
+	}
+
+	public List<ReviewComment> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<ReviewComment> comments) {
+		this.comments = comments;
+	}
+
+	@Override
+	public String toString() {
+		return "Review [id=" + id + ", content=" + content + ", active=" + active + ", dateCreated=" + dateCreated
+				+ ", dateUpdated=" + dateUpdated + ", dateVistied=" + dateVistied + ", location=" + location + ", user="
+				+ user + ", ratings=" + ratings + ", comments=" + comments + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Review other = (Review) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	
 
 }
--- JPACheckAhead/src/main/java/com/skilldistillery/checkahead/entities/ReviewRating.java
+++ JPACheckAhead/src/main/java/com/skilldistillery/checkahead/entities/ReviewRating.java
@@ -3,6 +3,10 @@ package com.skilldistillery.checkahead.entities;
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -16,32 +20,29 @@ public class ReviewRating {
 	
 	@Column(name="rating_value")
 	private int ratingValue;
+	
+	@ManyToOne
+	@JoinColumn(name="review_id")
+	@MapsId(value = "reviewId")  
+	private Review review;
+	
+	@OneToOne
+	@JoinColumn(name = "rating_id")
+	@MapsId(value = "ratingId")  
+	private Rating rating;
 
 	// m e t h o d s
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
+	public ReviewRating() {
+		super();
 	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		ReviewRating other = (ReviewRating) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
+	
+	public ReviewRating(ReviewRatingId id, int ratingValue, Review review, Rating rating) {
+		super();
+		this.id = id;
+		this.ratingValue = ratingValue;
+		this.review = review;
+		this.rating = rating;
 	}
 
 	public ReviewRatingId getId() {
@@ -60,23 +61,53 @@ public class ReviewRating {
 		this.ratingValue = ratingValue;
 	}
 
-	public ReviewRating(ReviewRatingId id, int ratingValue) {
-		super();
-		this.id = id;
-		this.ratingValue = ratingValue;
+	public Review getReview() {
+		return review;
 	}
 
-	public ReviewRating() {
-		super();
+	public void setReview(Review review) {
+		this.review = review;
+	}
+
+	public Rating getRating() {
+		return rating;
+	}
+
+	public void setRating(Rating rating) {
+		this.rating = rating;
 	}
 
 	@Override
 	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("ReviewRating [id=").append(id).append(", ratingValue=").append(ratingValue).append("]");
-		return builder.toString();
+		return "ReviewRating [id=" + id + ", ratingValue=" + ratingValue + ", review=" + review + ", rating=" + rating
+				+ "]";
 	}
 	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ReviewRating other = (ReviewRating) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 	
 	
 }
