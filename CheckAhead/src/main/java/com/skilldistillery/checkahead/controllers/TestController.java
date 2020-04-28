04ddc487833e2fd327922e264332a664fb91cc22
Frank Martin <cedricmrtn@gmail.com>
2020-04-28 12:35:21 -0500

    Stub angular project, added RequestMapping and CrossOrigin in test controller

--- CheckAhead/src/main/java/com/skilldistillery/checkahead/controllers/TestController.java
+++ CheckAhead/src/main/java/com/skilldistillery/checkahead/controllers/TestController.java
@@ -1,9 +1,13 @@
 package com.skilldistillery.checkahead.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping ("api")
+@CrossOrigin({"*", "http://localhost:4220"}) // Angular local port
 public class TestController {
 	
 	@GetMapping("ping")
