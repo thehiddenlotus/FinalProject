cca951f29336edcf1406691d3e420af2bcdff118
Luke Sutherland <luke.sutherland@live.com>
2020-04-28 14:04:38 -0600

    Address and User entites and tests with working relationship

--- JPACheckAhead/src/test/java/com/skilldistillery/checkahead/entities/AddressTest.java
+++ JPACheckAhead/src/test/java/com/skilldistillery/checkahead/entities/AddressTest.java
@@ -12,11 +12,11 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class UserTest {
+class AddressTest {
 	
 	private static EntityManagerFactory emf;
 	private EntityManager em;
-	private User user;
+	private Address address;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -31,21 +31,21 @@ class UserTest {
 	@BeforeEach
 	void setUp() throws Exception {
 		em = emf.createEntityManager();
-		user = em.find(User.class, 1);
+		address = em.find(Address.class, 1);
 				
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
 		em.close();
-		user = null;
+		address = null;
 	}
 
 	@Test
 	void test() {
-		assertNotNull(user);
-		assertEquals("admin", user.getUsername());
-		assertEquals("Aurora", user.getAddress().getCity());
+		assertNotNull(address);
+		assertEquals("3932 South Joplin Way", address.getAddress());
+		assertEquals("Aurora", address.getCity());
 	}
 
 }
