0956d3e6dc40135dc345e37e389263802ebef7af
Frank Martin <cedricmrtn@gmail.com>
2020-04-28 17:37:10 -0500

    Added @Service for UserServImpl

--- CheckAhead/src/main/java/com/skilldistillery/checkahead/services/UserServiceImpl.java
+++ CheckAhead/src/main/java/com/skilldistillery/checkahead/services/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.skilldistillery.checkahead.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.skilldistillery.checkahead.repositories.UserRepository;
 
+@Service
 public class UserServiceImpl implements UserService{
 
 		@Autowired
