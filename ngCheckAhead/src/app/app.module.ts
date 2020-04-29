3e8f3480cb26b2ca34848aa16fdc446c541c445f
Luke Sutherland <luke.sutherland@live.com>
2020-04-29 16:00:55 -0600

    stubbed out ng models and components

--- ngCheckAhead/src/app/app.module.ts
+++ ngCheckAhead/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { LocationDetailComponent } from './components/location-detail/location-detail.component';
+import { UserSettingsComponent } from './components/user-settings/user-settings.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    LocationDetailComponent,
+    UserSettingsComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
