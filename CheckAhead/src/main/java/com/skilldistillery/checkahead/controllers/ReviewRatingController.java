07506451c798c2a4d332184e4f176afc05c0f5e0
Diego Hoyos <hoyosdiegoa@gmail.com>
2020-04-29 11:53:02 -0600

    got reviewrating create and get working

---
+++ CheckAhead/src/main/java/com/skilldistillery/checkahead/controllers/ReviewRatingController.java
@@ -0,0 +1,52 @@
+package com.skilldistillery.checkahead.controllers;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.skilldistillery.checkahead.entities.Location;
+import com.skilldistillery.checkahead.entities.ReviewRating;
+import com.skilldistillery.checkahead.services.ReviewRatingService;
+
+@RestController
+@RequestMapping ("api")
+@CrossOrigin({"*", "http://localhost:4220"}) // Angular local port
+public class ReviewRatingController {
+	
+	@Autowired
+	private ReviewRatingService rrServ;
+	
+	@GetMapping("reviewratings")
+	public List<ReviewRating> getAllRRs(HttpServletResponse response){
+		List<ReviewRating> rrs = rrServ.findAllRRs();
+		if (rrs.size() > 0) {
+			return rrs;
+		}
+		else {
+			response.setStatus(404);
+			return null;
+		}
+	}
+	
+	@PostMapping("reviewratings/{ratingid}/{reviewid}")
+	public ReviewRating createNewRR(@PathVariable Integer ratingid, @PathVariable Integer reviewid, @RequestBody ReviewRating rr, HttpServletResponse response){
+		ReviewRating newRR = rrServ.createRR(reviewid, ratingid, rr);
+		if (newRR != null) {
+			return newRR;
+		}
+		else {
+			response.setStatus(404);
+			return null;
+		}
+	}
+
+}
--- CheckAhead/src/main/java/com/skilldistillery/checkahead/services/ReviewRatingService.java
+++ CheckAhead/src/main/java/com/skilldistillery/checkahead/services/ReviewRatingService.java
@@ -1,5 +1,13 @@
 package com.skilldistillery.checkahead.services;
 
+import java.util.List;
+
+import com.skilldistillery.checkahead.entities.ReviewRating;
+
 public interface ReviewRatingService {
+
+	List<ReviewRating> findAllRRs();
+
+	ReviewRating createRR(int reviewId, int ratingId, ReviewRating rr);
 
 }
