0634e637d37de8da97f499c118074f467f93bd86
dyoachum <dyoachum@gmail.com>
2020-04-29 09:19:19 -0600

    added rating service and address service

--- CheckAhead/src/main/java/com/skilldistillery/checkahead/services/AddressService.java
+++ CheckAhead/src/main/java/com/skilldistillery/checkahead/services/AddressService.java
@@ -1,5 +1,21 @@
 package com.skilldistillery.checkahead.services;
 
+import java.util.List;
+
+import com.skilldistillery.checkahead.entities.Address;
+
 public interface AddressService {
 
+	List<Address> findAllAddresses();
+	
+	Address findAddressById(int id);
+	
+	Address createAddress(Address address);
+
+	Address updateAddress(Address address);
+	
+	boolean deleteAddress(int id);
+	
+	
+	
 }
--- CheckAhead/src/main/java/com/skilldistillery/checkahead/services/AddressServiceImpl.java
+++ CheckAhead/src/main/java/com/skilldistillery/checkahead/services/AddressServiceImpl.java
@@ -1,8 +1,11 @@
 package com.skilldistillery.checkahead.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.skilldistillery.checkahead.entities.Address;
 import com.skilldistillery.checkahead.repositories.AddressRepository;
 
 @Service
@@ -10,5 +13,35 @@ public class AddressServiceImpl implements AddressService {
 	
 	@Autowired
 	AddressRepository addRepo;
+
+	@Override
+	public List<Address> findAllAddresses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Address findAddressById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Address createAddress(Address address) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Address updateAddress(Address address) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deleteAddress(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
