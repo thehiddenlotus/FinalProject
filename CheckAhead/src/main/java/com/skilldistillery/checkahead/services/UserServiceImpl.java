22a8815a7430cd9de99db4973475e360d941574d
Luke Sutherland <luke.sutherland@live.com>
2020-04-29 11:46:41 -0600

    User repo and svc crud functional

--- CheckAhead/src/main/java/com/skilldistillery/checkahead/services/UserServiceImpl.java
+++ CheckAhead/src/main/java/com/skilldistillery/checkahead/services/UserServiceImpl.java
@@ -1,12 +1,12 @@
 package com.skilldistillery.checkahead.services;
 
 import java.util.List;
-
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.skilldistillery.checkahead.entities.Address;
 import com.skilldistillery.checkahead.entities.User;
 import com.skilldistillery.checkahead.repositories.UserRepository;
 
@@ -34,26 +34,52 @@ public class UserServiceImpl implements UserService{
 		}
 
 		@Override
-		public List<User> findUserByName(String username) {
-			// TODO Auto-generated method stub
-			return null;
+		public User findUserByName(String username) {
+			User user = userRepo.findByUsername(username);
+			if(user != null) {
+				return user;
+			}
+			else {
+				return null;
+			}
 		}
 
 		@Override
-		public User createUser(User user) {
-			// TODO Auto-generated method stub
-			return null;
+		public User createUser(User user, Address address) {
+			user.setAddress(address);
+			User newUser = userRepo.saveAndFlush(user);
+			if (newUser != null) {
+				return newUser;
+			}
+				return null;	
 		}
 
 		@Override
-		public User updateUser(User user) {
-			// TODO Auto-generated method stub
+		public User updateUser(int id, User user) {
+			Optional<User> opt = userRepo.findById(id);
+			if (opt.isPresent()) {
+				User managed = opt.get();
+				managed.setId(id);
+				managed.setUsername(user.getUsername());
+				managed.setEmail(user.getEmail());
+				managed.setRole(user.getRole());
+				managed.setActive(user.isActive());
+				managed.setDateUpdated(user.getDateUpdated());
+				managed.setAddress(user.getAddress());
+				return userRepo.saveAndFlush(managed);
+			}
 			return null;
 		}
 
 		@Override
 		public boolean deleteUser(int id) {
-			// TODO Auto-generated method stub
-			return false;
+			boolean result = false;
+			Optional<User> user = userRepo.findById(id);
+			if (user.isPresent()) {
+				userRepo.deleteById(id);
+				result = true;
+			}
+			
+			return result;
 		}
 }
