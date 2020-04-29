c6302c3aa0cfcdf4b9b7337f4d075a737ab511f5
Luke Sutherland <luke.sutherland@live.com>
2020-04-28 21:55:42 -0600

    user service started

--- CheckAhead/src/main/java/com/skilldistillery/checkahead/services/UserService.java
+++ CheckAhead/src/main/java/com/skilldistillery/checkahead/services/UserService.java
@@ -1,5 +1,23 @@
 package com.skilldistillery.checkahead.services;
 
+import java.util.List;
+
+import com.skilldistillery.checkahead.entities.User;
+
 public interface UserService {
+
+	List<User> findAllUsers();
+
+	User findUserById(int id);
+
+	List<User> findUserByName(String username);
+
+	List<User> findUserByAddressId(int id);
+
+	User createUser(User user);
+
+	User updateUser(User user);
+
+	boolean deleteUser(int id);
 
 }
