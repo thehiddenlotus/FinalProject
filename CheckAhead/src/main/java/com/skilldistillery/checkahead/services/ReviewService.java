e85d67899aafe99e2e45b6e86ef79fb509ae66a9
Frank Martin <cedricmrtn@gmail.com>
2020-04-29 10:50:57 -0500

    Completed review findAll, FindByID and create review

--- CheckAhead/src/main/java/com/skilldistillery/checkahead/services/ReviewService.java
+++ CheckAhead/src/main/java/com/skilldistillery/checkahead/services/ReviewService.java
@@ -1,5 +1,18 @@
 package com.skilldistillery.checkahead.services;
 
+import java.util.List;
+
+import com.skilldistillery.checkahead.entities.Review;
+
 public interface ReviewService {
+	List<Review> findAllReviews();
+	
+	Review findById(Integer reviewId);
+	
+	Review createReview(String username, Review review);
+	
+	Review updateReview(Integer reviewId, Review review);
+	
+	boolean deleteReview(Integer reviewId);
 
 }
--- CheckAhead/src/main/java/com/skilldistillery/checkahead/services/ReviewServiceImpl.java
+++ CheckAhead/src/main/java/com/skilldistillery/checkahead/services/ReviewServiceImpl.java
@@ -1,13 +1,63 @@
 package com.skilldistillery.checkahead.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.skilldistillery.checkahead.entities.Review;
+import com.skilldistillery.checkahead.entities.User;
 import com.skilldistillery.checkahead.repositories.ReviewRepository;
+import com.skilldistillery.checkahead.repositories.UserRepository;
 
 @Service
 public class ReviewServiceImpl implements ReviewService {
 
 	@Autowired
-	private ReviewRepository repo;
+	private ReviewRepository reviewRepo;
+
+	@Autowired
+	private UserRepository userRepo;
+
+	@Override
+	public List<Review> findAllReviews() {
+		// TODO Auto-generated method stub
+		return reviewRepo.findAll();
+	}
+
+	@Override
+	public Review findById(Integer reviewId) {
+		Optional<Review> optReview = reviewRepo.findById(reviewId);
+
+		if (optReview.isPresent()) {
+			Review foundReview = optReview.get();
+			return foundReview;
+		}
+		return null;
+	}
+
+	@Override
+	public Review createReview(String username, Review review) {
+		User user = userRepo.findByUsername(username);
+		if (user != null) {
+			review.setUser(user);
+			reviewRepo.saveAndFlush(review);
+		} else {
+			review = null;
+		}
+		return review;
+	}
+
+	@Override
+	public Review updateReview(Integer reviewId, Review review) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean deleteReview(Integer reviewId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
