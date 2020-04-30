559ef3ae0960736f0ad783c6dd4484aab30d957a
Frank Martin <cedricmrtn@gmail.com>
2020-04-30 12:08:12 -0500

    Login page stubbed out. Wating for security config

--- ngCheckAhead/src/app/components/login/login.component.ts
+++ ngCheckAhead/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  // accessDenied = false;
+  constructor(
+    private router: Router
+    // private auth: AuthService
+  ) { }
 
   ngOnInit(): void {
   }
+
+  // login(form: NgForm) {
+  //   const user = form.value;
+  //   this.auth.login(user.username, user.password).subscribe(
+  //     success => {
+  //       console.log('LoginComponent.login(): Logged in');
+
+  //       this.router.navigate(['/search']);
+  //     },
+  //     failed => {
+  //       console.log('LoginComponent.login(): Login Failed');
+  //       this.accessDenied = true;
+  //       this.router.navigateByUrl('/login');
+  //     }
+  //   );
+  // }
 
 }
