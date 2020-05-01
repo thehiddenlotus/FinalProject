6180d3805d5c6d4130cd2fb9806e06f805d14daf
dyoachum <dyoachum@gmail.com>
2020-05-01 13:18:27 -0600

    services

--- ngCheckAhead/src/app/services/user.service.ts
+++ ngCheckAhead/src/app/services/user.service.ts
@@ -1,9 +1,74 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-
-  constructor() { }
+  private url = environment.baseUrl + 'api/user'
+  private user : User [] = [];
+  constructor(
+    private http: HttpClient
+  ) { }
+  public index() {
+    const httpOptions = this.getHttpOptions();
+    return this.http.get<User[]>(this.url, httpOptions).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError('UserService.index: error retrieving user: ' + err);
+      })
+    );
+  }
+  public show(id) {
+    const httpOptions = this.getHttpOptions();
+    return this.http.get<User>(`${this.url}/${id}`, httpOptions).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError('UserService.show: error retrieving entry: ' + err);
+      })
+    );
+  }
+  public create(user: User) {
+   this.user.push(user);
+    const httpOptions = this.getHttpOptions();
+    return this.http.post<User>(this.url, user, httpOptions).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError('UserService.create: error creating entry: ' + err);
+      })
+    );
+  }
+  public update(user: User) {
+    const httpOptions = this.getHttpOptions();
+    return this.http
+      .put<User>(`${this.url}/${user.id}`, User, httpOptions)
+      .pipe(
+        catchError((err: any) => {
+          console.log(err);
+          return throwError('UserService.update: error updating todo: ' + err);
+        })
+      );
+  }
+  public destroy(id: number) {
+    const httpOptions = this.getHttpOptions();
+    return this.http.delete(`${this.url}/${id}`, httpOptions).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError('UserService.delete: error deleting entry: ' + err);
+      })
+    );
+  }
+  private getHttpOptions() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Athorization': 'my-auth-token'
+      })
+    };
+    return httpOptions;
+  }
 }
