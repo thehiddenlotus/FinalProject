1e9919e9eae8b53b70c35dc4a1f3cb347e00b60e
Frank Martin <cedricmrtn@gmail.com>
2020-05-01 11:39:44 -0500

    Login methods for frontend completed

--- ngCheckAhead/src/app/components/login/login.component.ts
+++ ngCheckAhead/src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,29 +9,29 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  // accessDenied = false;
+  accessDenied = false;
   constructor(
-    private router: Router
-    // private auth: AuthService
+    private router: Router,
+    private auth: AuthService
   ) { }
 
   ngOnInit(): void {
   }
 
-  // login(form: NgForm) {
-  //   const user = form.value;
-  //   this.auth.login(user.username, user.password).subscribe(
-  //     success => {
-  //       console.log('LoginComponent.login(): Logged in');
+  login(form: NgForm) {
+    const user = form.value;
+    this.auth.login(user.username, user.password).subscribe(
+      success => {
+        console.log('LoginComponent.login(): Logged in');
 
-  //       this.router.navigate(['/search']);
-  //     },
-  //     failed => {
-  //       console.log('LoginComponent.login(): Login Failed');
-  //       this.accessDenied = true;
-  //       this.router.navigateByUrl('/login');
-  //     }
-  //   );
-  // }
+        this.router.navigate(['/search']);
+      },
+      failed => {
+        console.log('LoginComponent.login(): Login Failed');
+        this.accessDenied = true;
+        this.router.navigateByUrl('/login');
+      }
+    );
+  }
 
 }
