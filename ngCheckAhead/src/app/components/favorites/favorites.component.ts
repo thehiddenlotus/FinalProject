f989a517138246a91065860085860ff7b3d4bf17
Diego Hoyos <hoyosdiegoa@gmail.com>
2020-05-05 12:39:45 -0600

    finished display of average for locations search

--- ngCheckAhead/src/app/components/favorites/favorites.component.ts
+++ ngCheckAhead/src/app/components/favorites/favorites.component.ts
@@ -19,7 +19,7 @@ export class FavoritesComponent implements OnInit, OnChanges {
   @Input() searchKey: string;
 
   favorites: Location[];
-  // reviewRatings: ReviewRating[];
+  reviewRatings: ReviewRating[];
 
   constructor(
     private locSvc: LocationService,
@@ -31,7 +31,7 @@ export class FavoritesComponent implements OnInit, OnChanges {
     this.locSvc.index().subscribe(
       data => {
         this.favorites = data;
-        // this.getAverages();
+        this.getAverages();
       },
       error => {
         console.log("error in favorites index");
@@ -53,79 +53,77 @@ export class FavoritesComponent implements OnInit, OnChanges {
     }
   }
 
-  // //function to populate reviewRatings to get averages
-  // populateReviewRatings(id: number): void {
-  //   this.rrServ.findByLocation(id).subscribe(
-  //     good => {
-  //       this.reviewRatings = good;
-  //       this.populateAverages(this.reviewRatings, id)
-  //     },
-  //     error => {
-  //       console.log("error in populating reviewRatings in location-detail");
-  //       console.log(error);
-  //       this.populateAverages(this.reviewRatings, id)
-  //     }
-  //   )
-  // }
+  //function to populate reviewRatings to get averages
+  populateReviewRatings(id: number, j: number): void {
+    this.rrServ.findByLocation(id).subscribe(
+      good => {
+        this.reviewRatings = good;
+        this.populateAverages(this.reviewRatings, j)
+      },
+      error => {
+        console.log("error in populating reviewRatings in location-detail");
+        console.log(error);
+        this.populateAverages(this.reviewRatings, j)
+      }
+    )
+  }
 
-  // //function to get averages for current store
-  // getAverages(): void {
+  //function to get averages for current store
+  getAverages(): void {
 
-  //   //go through values and add to arrays
-  //   for (let j = 0; j < this.favorites.length; j++) {
-  //     this.populateReviewRatings(this.favorites[j].id);
-  //   }
-  // }
+    //go through values and add to arrays
+    for (let j = 0; j < this.favorites.length; j++) {
+      this.populateReviewRatings(this.favorites[j].id, j);
+    }
+  }
 
-  // populateAverages(reviews: ReviewRating[], id:number){
-  //   var cleanlinessA = [];
-  //   var trafficA = [];
-  //   var checkoutA = [];
-  //   var stockA = [];
-  //   if (this.reviewRatings.length > 0) {
-  //     console.log('inside if')
-  //     for (let i = 0; i < this.reviewRatings.length; i++) {
-  //       if (this.reviewRatings[i].id.ratingId === 1) {
-  //         cleanlinessA.push(this.reviewRatings[i].ratingValue);
-  //       }
-  //       else if (this.reviewRatings[i].id.ratingId === 2) {
-  //         trafficA.push(this.reviewRatings[i].ratingValue);
-  //       }
-  //       else if (this.reviewRatings[i].id.ratingId === 3) {
-  //         checkoutA.push(this.reviewRatings[i].ratingValue);
-  //       }
-  //       else if (this.reviewRatings[i].id.ratingId === 4) {
-  //         stockA.push(this.reviewRatings[i].ratingValue);
-  //       }
-  //     }
+  populateAverages(reviews: ReviewRating[], id:number){
+    var cleanlinessA = [];
+    var trafficA = [];
+    var checkoutA = [];
+    var stockA = [];
+    if (this.reviewRatings.length > 0) {
+      console.log('inside if')
+      for (let i = 0; i < this.reviewRatings.length; i++) {
+        if (this.reviewRatings[i].id.ratingId === 1) {
+          cleanlinessA.push(this.reviewRatings[i].ratingValue);
+        }
+        else if (this.reviewRatings[i].id.ratingId === 2) {
+          trafficA.push(this.reviewRatings[i].ratingValue);
+        }
+        else if (this.reviewRatings[i].id.ratingId === 3) {
+          checkoutA.push(this.reviewRatings[i].ratingValue);
+        }
+        else if (this.reviewRatings[i].id.ratingId === 4) {
+          stockA.push(this.reviewRatings[i].ratingValue);
+        }
+      }
       
 
-  //     //calculate avg or and return else zero
-  //     let rating = new AverageRatings();
-  //     let sum = cleanlinessA.length > 0 ? cleanlinessA.reduce((previous, current) => current += previous) : 0;
-  //     rating.cleanliness = cleanlinessA.length > 0 ? sum / cleanlinessA.length : 0;
-  //     sum = trafficA.length > 0 ? trafficA.reduce((previous, current) => current += previous) : 0;
-  //     rating.traffic = trafficA.length > 0 ? sum / trafficA.length : 0;
-  //     sum = checkoutA.length > 0 ? checkoutA.reduce((previous, current) => current += previous) : 0;
-  //     rating.checkout = checkoutA.length > 0 ? sum / checkoutA.length : 0;
-  //     sum = stockA.length > 0 ? stockA.reduce((previous, current) => current += previous) : 0;
-  //     rating.stock = stockA.length > 0 ? sum / stockA.length : 0;
-  //     this.favorites[id].averageRatings = rating;
-  //     console.log(this.favorites[id].averageRatings)
+      //calculate avg or and return else zero
+      let rating = new AverageRatings();
+      let sum = cleanlinessA.length > 0 ? cleanlinessA.reduce((previous, current) => current += previous) : 0;
+      rating.cleanliness = cleanlinessA.length > 0 ? sum / cleanlinessA.length : 0;
+      sum = trafficA.length > 0 ? trafficA.reduce((previous, current) => current += previous) : 0;
+      rating.traffic = trafficA.length > 0 ? sum / trafficA.length : 0;
+      sum = checkoutA.length > 0 ? checkoutA.reduce((previous, current) => current += previous) : 0;
+      rating.checkout = checkoutA.length > 0 ? sum / checkoutA.length : 0;
+      sum = stockA.length > 0 ? stockA.reduce((previous, current) => current += previous) : 0;
+      rating.stock = stockA.length > 0 ? sum / stockA.length : 0;
+      this.favorites[id].averageRatings = rating;
+      console.log(this.favorites[id].averageRatings)
 
-  //   }
-  //   else {
-  //     let rating = new AverageRatings();
-  //     rating.cleanliness = 0;
-  //     rating.traffic = 0;
-  //     rating.checkout = 0;
-  //     rating.stock = 0;
-  //     this.favorites[id].averageRatings = rating;
-  //   }
-  //   console.log(this.favorites[id])
-  // }
-
-
+    }
+    else {
+      let rating = new AverageRatings();
+      rating.cleanliness = 0;
+      rating.traffic = 0;
+      rating.checkout = 0;
+      rating.stock = 0;
+      this.favorites[id].averageRatings = rating;
+    }
+    console.log(this.favorites[id])
+  }
 
 }
 
